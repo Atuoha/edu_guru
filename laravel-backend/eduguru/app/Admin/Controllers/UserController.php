ebf561b445517c5ba4a44ed724f3f96d4384a97b
Atuoha <atuohainitiatives@gmail.com>
2023-10-04 10:56:47 +0100

    viewing users through laravel admin [routing, disabling actions and buttons]

---
+++ laravel-backend/eduguru/app/Admin/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class UserController extends AdminController
+{
+    protected $title ='Users';
+
+    protected function grid()
+    {
+        $grid = new Grid(new User());
+
+        $grid->column('id', __('Id'));
+        $grid->column('name', __('Name'));
+        $grid->column('email', __('Email'));
+        $grid->column('created_at', __('Created at'));
+        $grid->disableActions();
+        $grid->disableCreateButton();
+        $grid->disableExport();
+
+        return $grid;
+    }
+
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('name', __('Name'));
+        $show->field('email', __('Email'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    protected function form()
+    {
+        $form = new Form(new User());
+
+        $form->textarea('name', __('Name'));
+        $form->textarea('email', __('Email'));
+
+        return $form;
+    }
+}
